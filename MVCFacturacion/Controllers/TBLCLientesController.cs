acd79435c67ac5e42573262337a2b7804b7432ce
sidnyzh <syzapataho@gmail.com>
2025-05-31 20:17:58 -0500

    Agregando el CRUD de TBLClientes y TBLProductos

--- MVCFacturacion/Controllers/TBLCLientesController.cs
+++ MVCFacturacion/Controllers/TBLCLientesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVCFacturacion.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,8 +12,112 @@ namespace MVCFacturacion.Controllers
         // GET: TBLCLientes
         public ActionResult Index()
         {
+            using (DBFacturacion db = new DBFacturacion())
+            {
+                var clientes = db.TBLCLIENTES.ToList();
+                return View(clientes);
+            }
+        }
+
+        public ActionResult Nuevo()
+        {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Nuevo(FormCollection collection)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (DBFacturacion db = new DBFacturacion())
+                    {
+                        var nuevoCliente = new TBLCLIENTES
+                        {
+                            StrNombre = collection["StrNombre"],
+                            NumDocumento = long.Parse(collection["NumDocumento"]),
+                            StrDireccion = collection["StrDireccion"],
+                            StrTelefono = collection["StrTelefono"],
+                            StrEmail = collection["StrEmail"],
+                            DtmFechaModifica = DateTime.Now,
+                            StrUsuarioModifica = "javier"
+                        };
+
+                        db.TBLCLIENTES.Add(nuevoCliente);
+                        db.SaveChanges();
+                    }
+                    return RedirectToAction("Index");
+                }
+
+                return View(collection);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public ActionResult Editar(int id)
+        {
+            using (DBFacturacion db = new DBFacturacion())
+            {
+                var cliente = db.TBLCLIENTES.Find(id);
+                return View(cliente);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Editar(TBLCLIENTES model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (DBFacturacion db = new DBFacturacion())
+                    {
+                        var cliente = db.TBLCLIENTES.Find(model.IdCliente);
+                        cliente.StrNombre = model.StrNombre;
+                        cliente.NumDocumento = model.NumDocumento;
+                        cliente.StrDireccion = model.StrDireccion;
+                        cliente.StrTelefono = model.StrTelefono;
+                        cliente.StrEmail = model.StrEmail;
+                        cliente.DtmFechaModifica = DateTime.Now;
+                        cliente.StrUsuarioModifica = "javier";
+
+                        db.Entry(cliente).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                    }
+
+                    return RedirectToAction("Index");
+                }
+
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Borrar(int id)
+        {
+            try
+            {
+                using (DBFacturacion db = new DBFacturacion())
+                {
+                    var cliente = db.TBLCLIENTES.Find(id);
+                    db.TBLCLIENTES.Remove(cliente);
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return RedirectToAction("Index");
+        }
     }
-}
+}   
--- MVCFacturacion/Controllers/TBLProductosController.cs
+++ MVCFacturacion/Controllers/TBLProductosController.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using MVCFacturacion.Models;
+using System;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace MVCFacturacion.Controllers
@@ -11,7 +11,117 @@ namespace MVCFacturacion.Controllers
         // GET: TBLProductos
         public ActionResult Index()
         {
-            return View();
+            using (var db = new DBFacturacion())
+            {
+                var productos = db.TBLPRODUCTO
+                         .Include(p => p.TBLCATEGORIA_PROD) 
+                         .ToList();
+                return View(productos);
+            }
+        }
+        public ActionResult Nuevo()
+        {
+            using (var db = new DBFacturacion())
+            {
+                ViewBag.IdCategoria = new SelectList(db.TBLCATEGORIA_PROD.ToList(), "IdCategoria", "StrDescripcion");
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Nuevo(TBLPRODUCTO model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (var db = new DBFacturacion())
+                    {
+                        model.DtmFechaModifica = DateTime.Now;
+                        model.StrUsuarioModifica = "javier";
+                        db.TBLPRODUCTO.Add(model);
+                        db.SaveChanges();
+                    }
+                    return RedirectToAction("Index");
+                }
+
+                using (var db = new DBFacturacion())
+                {
+                    ViewBag.IdCategoria = new SelectList(db.TBLCATEGORIA_PROD.ToList(), "IdCategoria", "StrDescripcion");
+                    return View(model);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public ActionResult Editar(int id)
+        {
+            using (var db = new DBFacturacion())
+            {
+                var producto = db.TBLPRODUCTO.Find(id);
+                ViewBag.IdCategoria = new SelectList(db.TBLCATEGORIA_PROD.ToList(), "IdCategoria", "StrDescripcion");
+                return View(producto);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Editar(TBLPRODUCTO model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (var db = new DBFacturacion())
+                    {
+                        var producto = db.TBLPRODUCTO.Find(model.IdProducto);
+                        producto.StrNombre = model.StrNombre;
+                        producto.StrCodigo = model.StrCodigo;
+                        producto.NumPrecioCompra = model.NumPrecioCompra;
+                        producto.NumPrecioVenta = model.NumPrecioVenta;
+                        producto.IdCategoria = model.IdCategoria;
+                        producto.StrDetalle = model.StrDetalle;
+                        producto.strFoto = model.strFoto;
+                        producto.NumStock = model.NumStock;
+                        producto.DtmFechaModifica = DateTime.Now;
+                        producto.StrUsuarioModifica = "javier";
+
+                        db.Entry(producto).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                    }
+                    return RedirectToAction("Index");
+                }
+
+                using (var db = new DBFacturacion())
+                {
+                    ViewBag.IdCategoria = new SelectList(db.TBLCATEGORIA_PROD.ToList(), "IdCategoria", "StrNombre", model.IdCategoria);
+                    return View(model);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public ActionResult Borrar(int id)
+        {
+            try
+            {
+                using (var db = new DBFacturacion())
+                {
+                    var producto = db.TBLPRODUCTO.Find(id);
+                    db.TBLPRODUCTO.Remove(producto);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
     }
 }
